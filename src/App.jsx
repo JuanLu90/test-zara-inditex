ab95ae427db60560102c28d129b0631ce90dbc60
Juan Luis <juanlu.calderon@gmail.com>
2024-07-24 13:21:46 +0200

    refactor

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+
+// CONTEXT
+import { AppContextProvider } from "./context/app.context";
+
+// COMPONENTS
+import Layout from "./components/layout/layout.component";
+import CharactersList from "./views/characters-list/characters-list.component";
+import FavoritesList from "./views/favorites-list/favorites-list.component";
+import CharacterDetails from "./views/character-details/character-details.component";
+
+import { theme } from "./utils/styled";
+
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <AppContextProvider>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<CharactersList />} />
+              <Route path="/favorites" element={<FavoritesList />} />
+              <Route path="/details/:id" element={<CharacterDetails />} />
+            </Routes>
+          </Layout>
+        </AppContextProvider>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+};
+
+export default App;
---
+++ src/views/character-details/character-details.component.jsx
@@ -0,0 +1,127 @@
+// HOOKS
+import { useEffect } from "react";
+import useCharacterDetailstHook from "./character-details.hook";
+import { useParams } from "react-router-dom";
+import {
+  CharacterDetailsWrapper,
+  CharacterDetailsInfoWrapper,
+  CharacterDetailsImage,
+  CharacterDetailsInfo,
+  CharacterDetailsComicsWrapper,
+  ComicWrapper,
+  ComicImageWrapper,
+  CharacterDetailsComicsList,
+  ComicTitle,
+} from "./character-details.styled";
+
+// CONTEXT
+import { useAppContext } from "../../context/app.context";
+
+// IMAGES
+import HeartUnfillIcon from "../../assets/heart-unfill.png";
+import HeartUIcon from "../../assets/heart.png";
+import Loader from "../../components/loader/loader.component";
+
+const CharacterDetailsList = () => {
+  const { characterDetails, isFetching, handleGetCharacterDetails } =
+    useCharacterDetailstHook();
+
+  const {
+    favouriteCharactersListStoraged,
+    setFavouriteCharactersListStoraged,
+  } = useAppContext();
+
+  const { id } = useParams();
+  useEffect(() => {
+    handleGetCharacterDetails(id);
+  }, []);
+
+  const findFavoriteFn = () =>
+    favouriteCharactersListStoraged.some((x) => x.id == characterDetails.id);
+
+  const handleOnClickFav = (id) => {
+    const matchCharacter = favouriteCharactersListStoraged.findIndex(
+      (z) => z.id == id
+    );
+
+    if (matchCharacter !== -1) {
+      favouriteCharactersListStoraged.splice(matchCharacter, 1);
+      return setFavouriteCharactersListStoraged([
+        ...favouriteCharactersListStoraged,
+      ]);
+    } else
+      return setFavouriteCharactersListStoraged((prevArr) => [
+        ...prevArr,
+        characterDetails,
+      ]);
+  };
+
+  console.log(isFetching);
+
+  return (
+    <CharacterDetailsWrapper>
+      {isFetching ? (
+        <Loader />
+      ) : (
+        <>
+          <CharacterDetailsInfoWrapper>
+            <div>
+              <CharacterDetailsImage>
+                <img
+                  src={`${characterDetails.path}.${characterDetails.extension}`}
+                  alt={characterDetails.name}
+                  title={characterDetails.name}
+                />
+              </CharacterDetailsImage>
+              <CharacterDetailsInfo>
+                <div>
+                  <h1>{characterDetails.name}</h1>
+                  <button onClick={() => handleOnClickFav(characterDetails.id)}>
+                    <img
+                      src={findFavoriteFn() ? HeartUIcon : HeartUnfillIcon}
+                      alt={
+                        findFavoriteFn()
+                          ? "Delete from favorites"
+                          : "Add to favorites"
+                      }
+                      title={
+                        findFavoriteFn()
+                          ? "Delete from favorites"
+                          : "Add to favorites"
+                      }
+                    />
+                  </button>
+                </div>
+                <p>{characterDetails.description}</p>
+              </CharacterDetailsInfo>
+            </div>
+          </CharacterDetailsInfoWrapper>
+          <CharacterDetailsComicsWrapper>
+            <div>
+              <h1>COMICS</h1>
+              <CharacterDetailsComicsList>
+                {characterDetails?.comics?.map((comic) => {
+                  return (
+                    <ComicWrapper key={comic.id}>
+                      <ComicImageWrapper>
+                        <img
+                          src={`${comic.path}.${comic.extension}`}
+                          alt={comic.title}
+                          title={comic.title}
+                        />
+                      </ComicImageWrapper>
+                      <ComicTitle>{comic.title}</ComicTitle>
+                      <span>{comic.date}</span>
+                    </ComicWrapper>
+                  );
+                })}
+              </CharacterDetailsComicsList>
+            </div>
+          </CharacterDetailsComicsWrapper>
+        </>
+      )}
+    </CharacterDetailsWrapper>
+  );
+};
+
+export default CharacterDetailsList;
